2b239a0b1462cbdcda40e61ccf50a6e62b079300
Abraham Isaac Durán <aid.mest@gmail.com>
2020-07-19 01:11:04 -0400

    Initial commit

---
+++ Roomies.WebAPI/Models/Roomie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Roomies.WebAPI.Models
+{
+    public class Roomie
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
