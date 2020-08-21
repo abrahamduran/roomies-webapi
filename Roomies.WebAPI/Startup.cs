68945c475e1a2f0e21dbb45a188a0528a6b05565
Abraham Isaac Durán <aid.mest@gmail.com>
2020-08-21 02:34:57 -0400

    Add forwarded headers readers

--- Roomies.WebAPI/Startup.cs
+++ Roomies.WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -60,6 +61,10 @@ namespace Roomies.WebAPI
             };
             ConventionRegistry.Register("roomiesDbConventions", pack, x => true);
 
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
             app.UseHttpsRedirection();
 
             app.UseRouting();
