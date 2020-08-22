b8af1057a1bfd03134ca0a6cc8263bfb12e03041
Abraham Isaac Durán <aid.mest@gmail.com>
2020-08-22 04:36:18 -0400

    Add Transactions Repository Interface

---
+++ Roomies.WebAPI/Repositories/Interfaces/ITransactionsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Roomies.WebAPI.Models;
+
+namespace Roomies.WebAPI.Repositories.Interfaces
+{
+    public interface ITransactionsRepository
+    {
+        IEnumerable<Transaction> Get();
+    }
+}
