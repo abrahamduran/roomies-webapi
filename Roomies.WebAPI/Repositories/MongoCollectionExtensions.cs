ff126b11efbbe9f70fb6ee3f1192bd91cf45b795
Abraham Isaac Durán <aid.mest@gmail.com>
2020-08-22 20:14:00 -0400

    Refactor Index creation extension

--- Roomies.WebAPI/Repositories/MongoCollectionExtensions.cs
+++ Roomies.WebAPI/Repositories/MongoCollectionExtensions.cs
@@ -9,24 +9,31 @@ namespace Roomies.WebAPI.Repositories
         public static string CreateIndex<TDocument>(this IMongoCollection<TDocument> collection, Expression<Func<TDocument, object>> field)
         {
             var keys = Builders<TDocument>.IndexKeys.Ascending(field);
-            var options = new CreateIndexOptions();
-            var model = new CreateIndexModel<TDocument>(keys, options);
-            return collection.Indexes.CreateOne(model);
+            return collection.CreateIndex(keys, new CreateIndexOptions());
         }
 
         public static string CreateIndex<TDocument>(this IMongoCollection<TDocument> collection, FieldDefinition<TDocument> field)
         {
-
             var keys = Builders<TDocument>.IndexKeys.Ascending(field);
-            var options = new CreateIndexOptions();
-            var model = new CreateIndexModel<TDocument>(keys, options);
-            return collection.Indexes.CreateOne(model);
+            return collection.CreateIndex(keys, new CreateIndexOptions());
         }
 
         public static string CreateUniqueIndex<TDocument>(this IMongoCollection<TDocument> collection, Expression<Func<TDocument, object>> field)
         {
             var keys = Builders<TDocument>.IndexKeys.Ascending(field);
             var options = new CreateIndexOptions() { Unique = true };
+            return collection.CreateIndex(keys, options);
+        }
+
+        public static string CreateUniqueIndex<TDocument>(this IMongoCollection<TDocument> collection, FieldDefinition<TDocument> field)
+        {
+            var keys = Builders<TDocument>.IndexKeys.Ascending(field);
+            var options = new CreateIndexOptions() { Unique = true };
+            return collection.CreateIndex(keys, options);
+        }
+
+        private static string CreateIndex<TDocument>(this IMongoCollection<TDocument> collection, IndexKeysDefinition<TDocument> keys, CreateIndexOptions options)
+        {
             var model = new CreateIndexModel<TDocument>(keys, options);
             return collection.Indexes.CreateOne(model);
         }
