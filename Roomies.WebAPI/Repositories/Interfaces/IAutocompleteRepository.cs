389cf4ae10128a4dd046a420ad2358bc59fa1f2f
Abraham Isaac Durán <aid.mest@gmail.com>
2020-08-24 15:33:53 -0400

    Add Autocomplete repository

---
+++ Roomies.WebAPI/Repositories/Interfaces/IAutocompleteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Roomies.WebAPI.Models;
+
+namespace Roomies.WebAPI.Repositories.Interfaces
+{
+    public interface IAutocompleteRepository
+    {
+        void Index(IEnumerable<Autocomplete> autocomplete);
+        IEnumerable<string> Search(string text, AutocompleteType? type);
+    }
+}
