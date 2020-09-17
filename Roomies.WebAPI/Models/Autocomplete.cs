17a50cc5ea1d837fa17984328e92d9441166b38f
Abraham Isaac Durán <aid.mest@gmail.com>
2020-09-16 22:34:38 -0400

    Implement equatable in Autocomplete

--- Roomies.WebAPI/Models/Autocomplete.cs
+++ Roomies.WebAPI/Models/Autocomplete.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Roomies.WebAPI.Models
 {
-    public class Autocomplete : Entity
+    public class Autocomplete : Entity, IEquatable<Autocomplete>
     {
         public string Text { get; set; }
         public AutocompleteType Type { get; set; }
+
+        public override bool Equals(object obj) => Equals(obj as Autocomplete);
+
+        public bool Equals(Autocomplete other)
+            =>  other != null &&
+                Id == other.Id &&
+                Text == other.Text &&
+                Type == other.Type;
+
+        public override int GetHashCode() => HashCode.Combine(Id, Text, Type);
+
+        public static bool operator ==(Autocomplete left, Autocomplete right)
+            => EqualityComparer<Autocomplete>.Default.Equals(left, right);
+
+        public static bool operator !=(Autocomplete left, Autocomplete right)
+            => !(left == right);
     }
 
     public enum AutocompleteType
