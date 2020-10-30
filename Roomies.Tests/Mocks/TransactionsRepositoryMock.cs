5976a015e594d835a4ed5b75e90dd7d7a26a18b3
Abraham Isaac Durán <aid.mest@gmail.com>
2020-10-30 04:20:33 -0400

    Add unit tests for Transactions endpoints

---
+++ Roomies.Tests/Mocks/TransactionsRepositoryMock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Roomies.WebAPI.Models;
+using Roomies.WebAPI.Repositories.Interfaces;
+
+namespace Roomies.Tests.Mocks
+{
+    public class TransactionsRepositoryMock : ITransactionsRepository
+    {
+        public IEnumerable<Transaction> Transactions { get; set; }
+
+        public IEnumerable<Transaction> Get() => Transactions;
+    }
+}
---
+++ Roomies.Tests/Unit Tests/TransactionsControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Roomies.Tests.Mocks;
+using Roomies.WebAPI.Controllers;
+using Roomies.WebAPI.Models;
+using Xunit;
+
+namespace Roomies.Tests.UnitTests
+{
+    public class TransactionsControllerTests
+    {
+        private readonly TransactionsRepositoryMock _transactions;
+
+        public TransactionsControllerTests()
+        {
+            _transactions = new TransactionsRepositoryMock();
+        }
+
+        [Fact]
+        public void Get_TransactionLists_ReturnsListOfTransactions()
+        {
+            // arrange
+            var controller = new TransactionsController(_transactions);
+            var expected = new List<Transaction>() { Mock.Models.SimpleExpense(), Mock.Models.DetailedExpense(), Mock.Models.Payment(), Mock.Models.Payment() };
+            _transactions.Transactions = expected;
+
+            // act
+            var result = controller.Get().Result;
+
+            // assert
+            var ok = Assert.IsType<OkObjectResult>(result);
+            var list = Assert.IsType<List<Transaction>>(ok.Value);
+            Assert.Equal(expected, list);
+        }
+    }
+}
