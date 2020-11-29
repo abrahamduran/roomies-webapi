5f40acb750e8fd0fbaa129b68a8c9f856d6991ce
Abraham Isaac Durán <aid.mest@gmail.com>
2020-11-29 08:07:36 -0400

    Present Total based on unpaid expenses

--- Roomies.WebAPI/Responses/RoommateExpenses.cs
+++ Roomies.WebAPI/Responses/RoommateExpenses.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Roomies.WebAPI.Models;
 
 namespace Roomies.WebAPI.Responses
 {
     public class RoommateExpenses
     {
         public IEnumerable<ExpenseResult> Expenses { get; set; }
-        public decimal YourTotal => Expenses.Sum(x => x.Total.GetValueOrDefault(0));
+        public decimal YourTotal => Expenses
+            .Where(x => x.Status == ExpenseStatus.Unpaid)
+            .Sum(x => x.Total.GetValueOrDefault(0));
     }
 }
