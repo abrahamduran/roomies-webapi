ccf48e8425ff236866cef4914cc69473ebbcea25
Abraham Isaac Durán <aid.mest@gmail.com>
2020-12-13 21:21:54 -0400

    Add Register Payment use case

---
+++ Roomies.App/Use Cases/Register Payment/RegisterPaymentHandler.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using Roomies.App.Extensions;
+using Roomies.App.Models;
+using Roomies.App.Persistence.Interfaces;
+
+namespace Roomies.App.UseCases.RegisterPayment
+{
+    public class RegisterPaymentHandler
+    {
+        private const decimal MAX_OFFSET_VALUE = 0.1M;
+
+        private readonly IPaymentsRepository _payments;
+        private readonly IExpensesRepository _expenses;
+        private readonly IRoommatesRepository _roommates;
+
+        public RegisterPaymentHandler(IPaymentsRepository payments, IExpensesRepository expenses, IRoommatesRepository roommates)
+        {
+            _payments = payments;
+            _expenses = expenses;
+            _roommates = roommates;
+        }
+
+        public RegisterPaymentResponse Execute(RegisterPaymentRequest payment)
+        {
+            var exception = new UseCaseException();
+
+            var roommates = _roommates.Get(new[] { payment.PaidTo, payment.PaidBy }).ToDictionary(x => x.Id);
+            if (!roommates.ContainsKey(payment.PaidBy))
+                exception.AddError("PaidBy", "The PaidBy field is invalid. Please review it.");
+            if (!roommates.ContainsKey(payment.PaidTo))
+                exception.AddError("PaidTo", "The PaidBy field is invalid. Please review it.");
+
+            var expenses = _expenses.Get(payment.ExpenseIds);
+            if (expenses.Count() != payment.ExpenseIds.Count() || !expenses.Any())
+            {
+                exception.AddError("ExpenseIds", "At least one expense is invalid. Please review them before submission.");
+                foreach (var expense in expenses.Where(x => !payment.ExpenseIds.Contains(x.Id)).ToList())
+                    exception.AddError("ExpenseIds", $"Invalid expense: {expense.Id}");
+            }
+
+            if (expenses.Any(x => x.Status == ExpenseStatus.Paid) || expenses.Any(x => x.Payments?.Any(p => p.By.Id == payment.PaidBy) ?? false))
+            {
+                exception.AddError("ExpenseIds", "At least one expense has already been paid. Please review them before submission.");
+                foreach (var expense in expenses.Where(x => x.Status == ExpenseStatus.Paid).ToList())
+                    exception.AddError("ExpenseIds", $"Paid expense: {expense.Id}");
+            }
+
+            if (exception.Errors.Any()) throw exception;
+
+            if (!expenses.ContainsPayer(payment.PaidBy))
+            {
+                exception.AddError("PaidBy", "The selected payer is invalid for the selected expenses.");
+                exception.AddError("ExpenseIds", "At least one expense does not contains the selected payer.");
+                foreach (var expense in expenses.Where(x => x.Status == ExpenseStatus.Paid).ToList())
+                    exception.AddError("ExpenseIds", $"Paid expense: {expense.Id}");
+            }
+            if (!expenses.ContainsPayee(payment.PaidTo))
+            {
+                exception.AddError("PaidTo", "The selected payee is invalid for the selected expenses.");
+                exception.AddError("ExpenseIds", "At least one expense does not contains the selected payee.");
+            }
+
+            if (exception.Errors.Any()) throw exception;
+
+            var totalExpense = expenses.TotalForPayer(payment.PaidBy);
+            if (payment.Amount < totalExpense || payment.Amount > (totalExpense + MAX_OFFSET_VALUE))
+            {
+                exception.AddError("Amount", "The amount introduced does not match with the total amount for the selected expenses.");
+                exception.AddError("Amount", "As of now, partial payments are not supported.");
+                exception.AddError("Amount", $"Payment amount: {payment.Amount}, expenses total: {totalExpense}, difference: {totalExpense - payment.Amount}");
+                throw exception;
+            }
+
+            var entity = new Payment
+            {
+                By = new Payee { Id = payment.PaidBy, Name = roommates[payment.PaidBy].Name },
+                To = new Payee { Id = payment.PaidTo, Name = roommates[payment.PaidTo].Name },
+                Expenses = expenses.Select(x => (ExpenseSummary)x).ToList(),
+                Description = payment.Description,
+                Total = payment.Amount,
+                Date = DateTime.Now
+            };
+
+            var result = _payments.Add(entity);
+            if (result == null) throw exception;
+
+            var payments = expenses.Select(x => {
+                var summary = (PaymentSummary)result;
+                summary.Amount = x.TotalForPayer(payment.PaidBy);
+                return new PaymentUpdate { ExpenseId = x.Id, Summary = summary };
+            }).ToList();
+            _roommates.UpdateBalance(payment.PaidBy, -payment.Amount);
+            _roommates.UpdateBalance(payment.PaidTo, payment.Amount);
+            _expenses.SetPayment(payments);
+            return toResponse(result, false);
+        }
+
+        private RegisterPaymentResponse toResponse(Payment payment, bool includesExpenses)
+            => RegisterPaymentResponse.ForPayment(payment, includesExpenses);
+    }
+}
---
+++ Roomies.App/Use Cases/Register Payment/RegisterPaymentResponse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Roomies.App.Models;
+
+namespace Roomies.App.UseCases.RegisterPayment
+{
+    public class RegisterPaymentResponse : IEquatable<RegisterPaymentResponse>
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public long Date { get; set; }
+        public decimal Total { get; set; }
+        public Payee By { get; set; }
+        public Payee To { get; set; }
+        public IEnumerable<ExpenseSummary> Expenses { get; set; }
+
+        public static RegisterPaymentResponse ForRoommate(Payment payment) =>
+            new RegisterPaymentResponse
+            {
+                Id = payment.Id,
+                Date = (long)payment.Date.Subtract(DateTime.UnixEpoch).TotalSeconds,
+                Description = payment.Description,
+                Expenses = payment.Expenses,
+                To = payment.To,
+                Total = payment.Total
+            };
+
+        public static RegisterPaymentResponse ForPayment(Payment payment, bool includeExpenses) =>
+            new RegisterPaymentResponse
+            {
+                By = payment.By,
+                Id = payment.Id,
+                Date = (long)payment.Date.Subtract(DateTime.UnixEpoch).TotalSeconds,
+                Description = payment.Description,
+                Expenses = includeExpenses ? payment.Expenses : null,
+                To = payment.To,
+                Total = payment.Total
+            };
+
+        public bool Equals([AllowNull] RegisterPaymentResponse other) =>
+            By == other?.By &&
+            Id == other?.Id &&
+            Date == other?.Date &&
+            Description == other?.Description &&
+            Expenses == other?.Expenses &&
+            To == other?.To &&
+            Total == other?.Total;
+    }
+}
